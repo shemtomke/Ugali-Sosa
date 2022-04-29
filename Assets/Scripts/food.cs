b85650cf5032683a747e1f14f7a775e696b6091e
Shem Tom <63319003+shemtomke@users.noreply.github.com>
2022-04-29 12:37:31 +0300

    Update

---
+++ Assets/Scripts/food.cs
@@ -0,0 +1,55 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class food : MonoBehaviour
+{
+    //array of foods
+    public GameObject[] foods;
+    public float minValueX, maxValueX, yBound;
+
+    //distance between 
+
+    //speed at which the foods should fall //this should increase overtime and become harder
+    public float speedToFall = 3f;
+    public float maxSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FallingFood());
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+    {
+        //increase speed of the items
+
+    }
+
+    public IEnumerator FallingFood()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 2f));
+
+        var wanted = Random.Range(minValueX, maxValueX);
+        var position = new Vector2(wanted, yBound);
+        int randomFood = Random.Range(0, foods.Length);
+
+        GameObject gameObject = Instantiate(foods[randomFood], position, Quaternion.identity);
+
+        //speed of the things falling
+        /*if (speedToFall< maxSpeed) 
+        {
+            speedToFall += 0.1f * Time.deltaTime;
+
+        }*/
+        
+
+        StartCoroutine(FallingFood());
+    }
+}
---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameManager : MonoBehaviour
+{
+    public player player;
+    public food food;
+
+    public bool gameIsPaused;
+    public GameObject GameOverScreen;
+    public GameObject WinScreen;
+    public GameObject pauseGameObject;
+
+    public Text leveltxt;
+    public int Level = 1;
+
+    private void Awake()
+    {
+        Level = PlayerPrefs.GetInt("level", 1);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameOver();
+        WinGame();
+
+        leveltxt.text = "LEVEL :" + " " + Level;
+    }
+
+    public void GameOver()
+    {
+        if(player.isGameOver)
+        {
+            pauseGameObject.SetActive(false);
+            //show gameover screen
+            GameOverScreen.SetActive(true);
+            //stop food from falling
+            
+
+            player.isMove = false;
+            Time.timeScale = 0;
+        }
+    }
+    public void WinGame()
+    {
+        if(player.score >= player.maxScore)
+        {
+            pauseGameObject.SetActive(false);
+            //win game 
+            WinScreen.SetActive(true);
+            //stop food from falling
+            
+
+            player.isMove = false;
+            Time.timeScale = 0;
+        }
+    }
+
+    public void RetryGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    //unlock next level
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Level++;
+        PlayerPrefs.SetInt("level", Level);
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
